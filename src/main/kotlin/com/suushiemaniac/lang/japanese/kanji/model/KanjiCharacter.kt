549686b9bb18b5e6e4cbd9e506f8d80a31cc464a
Gregor Billing <suushiemaniac@gmail.com>
2020-02-16 18:40:22 +0100

    Restructure resources folder

--- src/main/kotlin/com/suushiemaniac/lang/japanese/kanji/model/KanjiCharacter.kt
+++
@@ -1,5 +0,0 @@
-package com.suushiemaniac.lang.japanese.kanji.model
-
-data class KanjiCharacter(val kanji: Char, val strokeCount: Int, val radical: KanjiRadical?)
-
-data class KanjiRadical(val radicalSymbol: Char, val name: String)
--- src/main/kotlin/com/suushiemaniac/lang/japanese/kanji/model/Reading.kt
+++
@@ -1,7 +0,0 @@
-package com.suushiemaniac.lang.japanese.kanji.model
-
-data class Reading(val kanaReading: String, val type: ReadingType)
-
-enum class ReadingType {
-    ON, KUN
-}
