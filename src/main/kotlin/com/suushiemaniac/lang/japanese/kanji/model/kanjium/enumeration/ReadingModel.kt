5939a15b0ca5be6c139cd008f87212a2929fd448
Gregor Billing <suushiemaniac@gmail.com>
2020-02-16 18:08:10 +0100

    Allow skipping OnYomi historic data

--- src/main/kotlin/com/suushiemaniac/lang/japanese/kanji/model/kanjium/enumeration/ReadingModel.kt
+++ src/main/kotlin/com/suushiemaniac/lang/japanese/kanji/model/kanjium/enumeration/ReadingModel.kt
@@ -1,4 +1,4 @@
 package com.suushiemaniac.lang.japanese.kanji.model.kanjium.enumeration
 
-data class OnYomi(val kanaReading: String, val historic: ReadingEra)
+data class OnYomi(val kanaReading: String, val historic: ReadingEra? = null)
 data class KunYomi(val coreReading: String, val okurigana: String)
