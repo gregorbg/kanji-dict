42143acaa20588988cb09c849b1f68f72f324472
Gregor Billing <suushiemaniac@gmail.com>
2020-03-04 13:19:37 +0100

    Upgrade a lot of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.61"
-    kotlin("plugin.serialization") version "1.3.61"
+    kotlin("jvm") version "1.3.70"
+    kotlin("plugin.serialization") version "1.3.70"
+
     id("com.github.ben-manes.versions") version "0.27.0"
 }
 
@@ -15,12 +16,16 @@ repositories {
 }
 
 dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
     implementation(kotlin("stdlib-jdk8"))
 
     implementation("org.jetbrains.exposed:exposed-core:0.21.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.21.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.21.1")
     implementation("com.mariten:kanatools:1.3.0")
+    implementation("com.atilika.kuromoji:kuromoji-ipadic:0.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.14.0")
 
     runtimeOnly("com.h2database:h2:1.4.200")
