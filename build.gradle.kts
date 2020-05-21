5538c766abe0dda4c09b7c8a9652e7ad5b5e45ed
Gregor Billing <suushiemaniac@gmail.com>
2020-05-21 09:24:52 +0200

    Version updates!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.unbrokendome.gradle.plugins.xjc.XjcExtension
 import org.unbrokendome.gradle.plugins.xjc.XjcGenerate
 
 plugins {
-    kotlin("jvm") version "1.3.71"
-    kotlin("plugin.serialization") version "1.3.71"
+    kotlin("jvm") version "1.3.72"
+    kotlin("plugin.serialization") version "1.3.72"
 
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.github.ben-manes.versions") version "0.28.0"
     id("org.unbroken-dome.xjc") version "1.4.3"
 }
 
@@ -24,9 +23,9 @@ dependencies {
 
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("org.jetbrains.exposed:exposed-core:0.21.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.21.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.21.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.24.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.24.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.24.1")
     implementation("com.mariten:kanatools:1.3.0")
     implementation("com.atilika.kuromoji:kuromoji-ipadic:0.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
@@ -34,7 +33,7 @@ dependencies {
     implementation("io.ktor:ktor-client-apache:1.3.2")
     implementation("io.ktor:ktor-client-serialization-jvm:1.3.2")
     implementation("it.skrape:skrapeit-core:1.0.0-alpha6")
-    implementation("org.jsoup:jsoup:1.12.2")
+    implementation("org.jsoup:jsoup:1.13.1")
 
     implementation("javax.xml.bind:jaxb-api:2.3.1")
     add("xjcClasspath", "org.jvnet.jaxb2_commons:jaxb2-basics-annotate:1.1.0")
@@ -42,7 +41,7 @@ dependencies {
     implementation("com.sun.xml.bind:jaxb-impl:2.3.1")
 
     runtimeOnly("com.h2database:h2:1.4.200")
-    runtimeOnly("org.xerial:sqlite-jdbc:3.30.1")
+    runtimeOnly("org.xerial:sqlite-jdbc:3.31.1")
 }
 
 sourceSets {
@@ -57,7 +56,7 @@ sourceSets {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 
-    //dependsOn("xjcGenerate")
+    dependsOn("xjcGenerate")
 }
 
 tasks.withType<XjcGenerate> {
