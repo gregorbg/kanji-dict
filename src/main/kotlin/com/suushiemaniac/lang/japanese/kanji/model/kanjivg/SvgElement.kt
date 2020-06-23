c480d1b262897a1f7cad64b075d675171fada537
Gregor Billing <suushiemaniac@gmail.com>
2020-06-23 13:22:59 +0200

    Add functioning radical parts coloring

--- src/main/kotlin/com/suushiemaniac/lang/japanese/kanji/model/kanjivg/SvgElement.kt
+++ src/main/kotlin/com/suushiemaniac/lang/japanese/kanji/model/kanjivg/SvgElement.kt
@@ -1,27 +1,42 @@
 package com.suushiemaniac.lang.japanese.kanji.model.kanjivg
 
 import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 
 @Serializable
 sealed class SvgElement {
     abstract val id: String
 
-    abstract fun withRadicalColor(): SvgElement
+    abstract fun withRadicalColor(level: Int = 0): SvgElement
+
+    abstract fun withIdSuffix(suffix: String): SvgElement
+
+    abstract fun withStyle(addStyle: String): SvgElement
+    abstract fun withTransform(addTransform: String): SvgElement
 
     @Serializable
     @XmlSerialName("g", KanjiVG.SVG_NAMESPACE, KanjiVG.SVG_PREFIX)
     data class Group(
         override val id: String,
         val style: String? = null,
+        val transform: String? = null,
         @XmlSerialName("element", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val element: String? = null,
         @XmlSerialName("original", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val original: String? = null,
-        @XmlSerialName("position", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val position: Position? = null,
+        @XmlSerialName(
+            "position",
+            KanjiVG.KANJIVG_NAMESPACE,
+            KanjiVG.KANJIVG_PREFIX
+        ) @XmlElement(false) val position: Position? = null,
         @XmlSerialName("variant", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val variant: Boolean = false,
         @XmlSerialName("partial", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val partial: Boolean = false,
         @XmlSerialName("part", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val part: Int? = null,
         @XmlSerialName("number", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val number: Int? = null,
-        @XmlSerialName("radical", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val radical: Radical? = null,
+        @XmlSerialName(
+            "radical",
+            KanjiVG.KANJIVG_NAMESPACE,
+            KanjiVG.KANJIVG_PREFIX
+        ) @XmlElement(false) val radical: Radical? = null,
         @XmlSerialName("phon", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val phon: String? = null,
         @XmlSerialName(
             "tradForm",
@@ -35,12 +50,48 @@ sealed class SvgElement {
         ) val radicalForm: Boolean = false,
         val elements: List<SvgElement>
     ) : SvgElement() {
-        override fun withRadicalColor(): SvgElement {
-            val colorStyle =
-                if (radical == null) style else listOfNotNull(style, "stroke:${radical.color}").joinToString(";")
+        override fun withRadicalColor(level: Int): SvgElement {
+            if (radical == null) {
+                val coloredElements = elements
+                    .filter { level == 0 || it is Group }
+                    .map { it.withRadicalColor(level) }
 
-            val coloredElements = elements.map { it.withRadicalColor() }
-            return copy(style = colorStyle, elements = coloredElements)
+                return copy(elements = coloredElements)
+            }
+
+            val nestedRadicals = elements
+                .filterIsInstance<Group>()
+                .map { it.withIdSuffix("nested") }
+                .map { it.withTransform(makeTransformStyle(level + 1)) }
+                .map { it.withRadicalColor(level + 1) }
+
+            val coloredChildren = nestedRadicals + elements
+
+            return withStyle(makeColorStyle(radical.color))
+                .copy(elements = coloredChildren)
+        }
+
+        override fun withIdSuffix(suffix: String): Group {
+            val suffixedChildren = elements.map { it.withIdSuffix(suffix) }
+            return copy(id = "$id-$suffix", elements = suffixedChildren)
+        }
+
+        override fun withStyle(addStyle: String): Group {
+            val newStyle = listOfNotNull(style, addStyle).joinToString(";")
+            return copy(style = newStyle)
+        }
+
+        override fun withTransform(addTransform: String): Group {
+            val newTransform = listOfNotNull(transform, addTransform).joinToString(" ")
+            return copy(transform = newTransform)
+        }
+
+        companion object {
+            const val TRANSFORM_OFFSET = 4.2
+
+            private fun makeColorStyle(color: String) = "stroke:$color"
+            private fun makeTransformStyle(level: Int) =
+                "translate(${level * TRANSFORM_OFFSET},${level * TRANSFORM_OFFSET})"
         }
     }
 
@@ -51,8 +102,9 @@ sealed class SvgElement {
         val d: String,
         @XmlSerialName("type", KanjiVG.KANJIVG_NAMESPACE, KanjiVG.KANJIVG_PREFIX) val type: String? = null
     ) : SvgElement() {
-        override fun withRadicalColor(): SvgElement {
-            return this
-        }
+        override fun withRadicalColor(level: Int) = this
+        override fun withIdSuffix(suffix: String) = copy(id = "$id-$suffix")
+        override fun withStyle(addStyle: String) = this
+        override fun withTransform(addTransform: String) = this
     }
 }
